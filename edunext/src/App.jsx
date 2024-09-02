26e2cbb286b15334b63b74522dfa5c581c3eec64
swadha112 <swadha.khatod07@gmail.com>
2024-09-03 03:00:42 +0530

    users

--- edunext/src/App.jsx
+++ edunext/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Mynavbar from '../components/navbar';
+import Mynavbar from './components/navbar';
+import Users from './components/users';
 
 function App() {
   return (
+    <>
     <div>
       <Mynavbar />
     </div>
+     <div>
+     <Users />
+   </div>
+   </>
   );
 }
 
