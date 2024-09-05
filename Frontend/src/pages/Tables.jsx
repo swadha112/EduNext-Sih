1e2e1b1190533265c0eacf99ed8bdd7378d0934a
swadha112 <swadha.khatod07@gmail.com>
2024-09-05 23:05:47 +0530

    news in other domain

--- Frontend/src/pages/Tables.jsx
+++ Frontend/src/pages/Tables.jsx
@@ -7,7 +7,7 @@ const Tables = () => {
   return (
     <>
       <Breadcrumb pageName="Market Trends" />
-
+      <h1 className="text-lg text-gray-600 mb-6">Latest Job Trends in any domain you like.</h1>
       <div className="flex flex-col gap-10">
         <TableOne />
         {/* <TableTwo />
