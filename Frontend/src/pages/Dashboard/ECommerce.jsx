168c623b7fa25045d4aab5ea6233e5ef10be478f
Rashi Joisher <joisherrashi730@gmail.com>
2024-09-05 16:25:42 +0530

    Updated dashboard

--- Frontend/src/pages/Dashboard/ECommerce.jsx
+++ Frontend/src/pages/Dashboard/ECommerce.jsx
@@ -8,6 +8,13 @@ import Professional from '../../images/cards/prof.jpg';
 const ECommerce = () => {
   return (
     <>
+
+      {/* Header */}
+      <div className="text-left py-4">
+        <h1 className="text-5xl font-bold">Welcome to EduNext!</h1>
+      </div>
+
+
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         {/* Card with PNG Image and Title */}
         <CardDataStats title="High School">
@@ -52,9 +59,6 @@ const ECommerce = () => {
             <div className="text-center py-10 bg-gray-50">
               <h1 className="text-4xl font-bold mb-2">Your Journey, Our Expertise.</h1>
               <p className="text-lg text-gray-600 mb-6">Count on our expertise to make the right choices for your education and career.</p>
-              <button className="bg-black text-white py-2 px-4 rounded-full">
-                Explore all services
-              </button>
             </div>
 
       {/* Other components like ChartOne, ChartTwo, etc. */}
