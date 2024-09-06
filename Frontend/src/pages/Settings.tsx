b579fe55e4438c6df6d8f219af048a2d42fa5f24
atharvaupare <atharvaupare5@gmail.com>
2024-09-06 10:59:29 +0530

    ui chnages

--- Frontend/src/pages/Settings.tsx
+++ Frontend/src/pages/Settings.tsx
@@ -358,7 +358,7 @@ const Settings = () => {
                   </div>
 
                   <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
-                    <div className="w-full sm:w-1/2">
+                    <div className="w-full ">
                       <label
                         className="mb-3 block text-sm font-medium text-black dark:text-white"
                         htmlFor="bio"
