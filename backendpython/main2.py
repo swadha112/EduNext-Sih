e6820214a4ebc1e2433605ef66a41e231f2e6815
AahanShetye <aahanshetye@gmail.com>
2024-09-26 21:55:06 +0530

    GG

--- backendpython/main2.py
+++ backendpython/main2.py
@@ -123,3 +123,6 @@ async def process_video(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)  # Running on port 8001
+
+
+## extra details for Aahan's Commit
